66e6ae5a84635bdf5bf9aa769c8dbe8840a62f03
dast1 <dast1@dast1ws3.bfi.local>
2013-02-11 12:30:59 +0100

    First async version

--- play-test/app/controllers/Application.scala
+++ play-test/app/controllers/Application.scala
@@ -5,19 +5,40 @@ import play.api.mvc.Action
 import play.api.data.Form
 import play.api.data.Forms._
 import models.Calculator
-
+import scala.concurrent.Future
+import play.api.libs.concurrent.Execution.Implicits._
 
 object Application extends Controller {
-	def index = Action {
-	  Ok(views.html.index(calculateForm, "-"))
-	}
-		
-	val calculateForm = Form(
-			"expression" -> text
-	)
-	
-	def calculate = Action { implicit request =>
-	  val expr = calculateForm.bindFromRequest.get;
-	  Ok(views.html.index(calculateForm,  expr + " = " + Calculator.apply(expr)))
-	}
+  def index = Action {
+    Ok(views.html.index(calculateForm, "-"))
+  }
+
+  val calculateForm = Form(
+    "expression" -> text
+  )
+
+  def calculate = Action {
+    implicit request =>
+      val expr = calculateForm.bindFromRequest.get
+      val result =
+        try {
+          Calculator.apply(expr)
+        } catch {
+          case failure => {
+            failure.getMessage
+          }
+        }
+       Ok(views.html.index(calculateForm, expr + " = " + result))
+  }
+
+  def calculateAsync = Action {
+    implicit request =>
+      val expr = calculateForm.bindFromRequest.get
+      val futureResult = Future {
+        Calculator.apply(expr)
+      }
+      Async {
+        futureResult.map( result => Ok(views.html.index(calculateForm, expr + " = " + result)))
+      }
+  }
 }
