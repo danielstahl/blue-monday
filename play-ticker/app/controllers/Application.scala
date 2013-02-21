c8a21aca86c68235fd0183286e77b5fe8b880cc6
danielstahl <oe.daniel.stahl@gmail.com>
2013-02-22 00:08:23 +0100

    Embryo of actor

--- play-ticker/app/controllers/Application.scala
+++ play-ticker/app/controllers/Application.scala
@@ -7,9 +7,16 @@ import play.api.data.Forms._
 import scala.concurrent.Future
 import play.api.libs.concurrent.Execution.Implicits._
 import views.html.defaultpages.badRequest
+import play.api.libs.concurrent.Akka
+import play.api.Play.current
+import akka.actor.Props
+import models.MyActor
 
 object Application extends Controller {
+  val myActor = Akka.system.actorOf(Props[MyActor], name = "myactor")
+  
   def index = Action {
+    myActor ! "test"
     Ok(views.html.index())
   }
 
