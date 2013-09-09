b535b0a1bf8dfaad5c6b1077a7d0a725ef718101
danielstahl <oe.daniel.stahl@gmail.com>
2013-09-09 22:53:30 +0200

    Initial commit of orderbook akka

---
+++ orderbook-akka/src/main/scala/OrderSystem.scala
@@ -0,0 +1,83 @@
+import akka.actor.{ActorRef, Props, ActorSystem, Actor}
+import akka.pattern.ask
+import akka.pattern._
+import akka.util.Timeout
+import scala.concurrent.duration._
+import java.util.UUID
+
+case class OrderMessage(uuid: String, orderbook: String, volume: Long, price: Double) extends ClientMessage(uuid)
+class ClientMessage(uuid: String)
+case class OrderStatusMessage(orderbook: String, volume: Long, price: Double)
+
+
+class ClientSupervisorActor extends Actor {
+
+  def receive = {
+    case orderMessage @ OrderMessage(uuid, orderbook, volume, price) =>
+      context.child(uuid).getOrElse {
+        context.actorOf(Props[ClientActor](new ClientActor(uuid)), uuid)
+      } forward orderMessage
+
+    case orderStatusMessage: OrderStatusMessage =>
+      context.children foreach(_ ! orderStatusMessage)
+  }
+}
+
+class ClientActor(uuid: String) extends Actor {
+  lazy val marketActor = context.actorSelection("/user/marketActor")
+
+  def receive = {
+    case om: OrderMessage =>
+      println(s"Client $uuid send orderbook $om update")
+      marketActor ! om
+    case osm: OrderStatusMessage =>
+      println(s"Client $uuid got orderbook status $osm")
+    case default =>
+      println(s"$self does not understand: $default")
+  }
+}
+
+class MarketActor extends Actor {
+  def receive = {
+    case orderMessage @ OrderMessage(uuid, orderbook, volume, price) =>
+      context.child(orderbook).getOrElse {
+        context.actorOf(Props[OrderbookActor](new OrderbookActor(orderbook)))
+      } forward orderMessage
+    case default =>
+      println(s"$self does not understand: $default")
+
+  }
+}
+
+class OrderbookActor(orderbook: String) extends Actor {
+  lazy val clientSupervisorActor = context.actorSelection("/user/clientSupervisorActor")
+
+  def receive = {
+    case OrderMessage(uuid, orderbook, volume, price) =>
+      clientSupervisorActor ! OrderStatusMessage(orderbook, volume, price)
+    case default =>
+      println(s"$self does not understand: $default")
+  }
+}
+
+object ClientMain {
+  val system = ActorSystem("OrderSystem")
+  val clientSupervisor = system.actorOf(Props[ClientSupervisorActor], "clientSupervisorActor")
+  val marketActor = system.actorOf(Props[MarketActor], "marketActor")
+
+  def main(args: Array[String]) {
+    val client1 = "client1"
+    implicit val timeout = Timeout(5 seconds)
+    clientSupervisor ! OrderMessage(client1, "ERIC-B", 100, 10.4)
+    Thread.sleep(100)
+
+    val client2 = "client2"
+    clientSupervisor ! OrderMessage(client2, "ABB", 200, 10.4)
+    Thread.sleep(100)
+
+    clientSupervisor ! OrderMessage(client2, "ERIC-B", 300, 11.5)
+    Thread.sleep(100)
+
+    system.shutdown()
+  }
+}
