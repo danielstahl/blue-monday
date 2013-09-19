a36d3d9a3ca313ca9adfb2fb51bc0b0248f7bba5
danielstahl <oe.daniel.stahl@gmail.com>
2013-09-19 21:01:14 +0200

    Added remote actors

--- orderbook-akka/build.sbt
+++ orderbook-akka/build.sbt
@@ -6,5 +6,6 @@ scalaVersion := "2.10.2"
 
 resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
 
-libraryDependencies +=
-  "com.typesafe.akka" %% "akka-actor" % "2.2.1"
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.2.1",
+  "com.typesafe.akka" %% "akka-remote" % "2.2.1")
--- orderbook-akka/src/main/scala/OrderSystem.scala
+++ orderbook-akka/src/main/scala/OrderSystem.scala
@@ -5,13 +5,16 @@ import scala.language.postfixOps
 
 class ClientMessage(uuid: String)
 
-case class OrderMessage(uuid: String, orderbook: String, volume: Long, price: Double) extends ClientMessage(uuid)
+case class OrderMessage(orderbook: String, volume: Long, price: Double)
 
 case class OrderStatusMessage(orderbook: String, volume: Long, price: Double)
 
 case class ClientLogout(uuid: String) extends ClientMessage(uuid)
 
-trait CommonActor { actor: Actor =>
+case class ClientLogin(uuid: String) extends ClientMessage(uuid)
+
+trait CommonActor {
+  actor: Actor =>
   def commonReceive: Actor.Receive = {
     case default =>
       println(s"${actor.self} does not understand: $default")
@@ -25,28 +28,25 @@ trait CommonActor { actor: Actor =>
 
 class ClientSupervisorActor extends Actor with CommonActor {
   def myReceive = {
-    case orderMessage @ OrderMessage(uuid, orderbook, volume, price) =>
+    case clientLogin@ClientLogin(uuid) =>
       context.child(uuid).getOrElse {
         context.actorOf(Props[ClientActor](new ClientActor(uuid)), uuid)
-      } forward orderMessage
-
+      } forward clientLogin
     case orderStatusMessage: OrderStatusMessage =>
-      context.children foreach(_ ! orderStatusMessage)
+      context.children foreach (_ ! orderStatusMessage)
 
-    case clientLogout @ ClientLogout(uuid) =>
+    case clientLogout@ClientLogout(uuid) =>
       context.child(uuid).foreach(_.forward(clientLogout))
   }
 }
 
 class ClientActor(uuid: String) extends Actor with CommonActor {
-  lazy val marketActor = context.actorSelection("/user/marketActor")
 
   def myReceive = {
-    case om: OrderMessage =>
-      println(s"Client $uuid send orderbook $om update")
-      marketActor ! om
     case osm: OrderStatusMessage =>
       println(s"Client $uuid got orderbook status $osm")
+    case ClientLogin(uuid) =>
+      println(s"Client login as ${uuid}")
     case ClientLogout(uuid) =>
       println(s"Client ${uuid} in actor ${self} is logging out")
       context.stop(self)
@@ -55,7 +55,7 @@ class ClientActor(uuid: String) extends Actor with CommonActor {
 
 class MarketActor extends Actor with CommonActor {
   def myReceive = {
-    case orderMessage @ OrderMessage(uuid, orderbook, volume, price) =>
+    case orderMessage@OrderMessage(orderbook, volume, price) =>
       context.child(orderbook).getOrElse {
         context.actorOf(Props[OrderbookActor](new OrderbookActor(orderbook)))
       } forward orderMessage
@@ -66,7 +66,8 @@ class OrderbookActor(orderbook: String) extends Actor with CommonActor {
   lazy val clientSupervisorActor = context.actorSelection("/user/clientSupervisorActor")
 
   def myReceive = {
-    case OrderMessage(uuid, orderbook, volume, price) =>
+    case orderMessage@OrderMessage(orderbook, volume, price) =>
+      println(s"Orderbook $orderbook got update $orderMessage")
       clientSupervisorActor ! OrderStatusMessage(orderbook, volume, price)
   }
 }
@@ -79,25 +80,37 @@ object ClientMain {
   def main(args: Array[String]) {
     val client1 = "client1"
     implicit val timeout = Timeout(5 seconds)
-    clientSupervisor ! OrderMessage(client1, "ERIC-B", 100, 10.4)
+    clientSupervisor ! ClientLogin(client1)
+    marketActor ! OrderMessage("ERIC-B", 100, 10.4)
     Thread.sleep(100)
 
     val client2 = "client2"
-    clientSupervisor ! OrderMessage(client2, "ABB", 200, 10.4)
+    clientSupervisor ! ClientLogin(client2)
+    marketActor ! OrderMessage("ABB", 200, 10.4)
     Thread.sleep(100)
 
-    clientSupervisor ! OrderMessage(client2, "ERIC-B", 300, 11.5)
+    marketActor ! OrderMessage("ERIC-B", 300, 11.5)
     Thread.sleep(100)
-
 
     clientSupervisor ! ClientLogout(client1)
     Thread.sleep(100)
 
-    clientSupervisor ! OrderMessage(client2, "ABB", 400, 14.8)
+    marketActor ! OrderMessage("ABB", 400, 14.8)
     Thread.sleep(100)
 
     clientSupervisor ! ClientLogout(client2)
-    Thread.sleep(100)
+    Thread.sleep(20000)
+
+    system.shutdown()
+  }
+}
+
+object RemoteMain {
+  def main(args: Array[String]) = {
+    System.setProperty("akka.remote.netty.tcp.port", "2553")
+    val system = ActorSystem("OrderSystem")
+
+    Thread.sleep(20000)
 
     system.shutdown()
   }
